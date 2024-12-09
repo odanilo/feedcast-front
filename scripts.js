a15166704a106ab0f4a60e32be8e94c68ebcacec
Danilo <dan.nunes0@gmail.com>
2024-12-09 03:16:46 -0300

    feat: adiciona lógica inicial para listar episódios

--- scripts.js
+++ scripts.js
@@ -15,6 +15,116 @@ const $formAdicionarEpisodio = document.querySelector(
   '[data-js="form-adicionar-episodio"]'
 );
 
+const $listEpisodiosContainer = document.querySelector(
+  '[data-js="list-episodios"]'
+);
+
+/*
+  --------------------------------------------------------------------------------------
+  Funções para listar episódios
+  --------------------------------------------------------------------------------------
+*/
+const gerarCardEpisodioMarkup = ({ id, audio, capa, descricao, titulo }) => `
+  <li class="episodios__item" data-episodio-id="${id}">
+    <main class="episodios-item__main">
+      <figure class="episodios-item__cover cover">
+        <img
+          src="${capa}"
+          alt="Capa do episódio '${titulo}'"
+        />
+      </figure>
+
+      <article class="episodios-item__info">
+        <header class="episodios-item__header">
+          <h3>
+            ${titulo}
+          </h3>
+
+          <div class="episodios-item__actions">
+            <button class="cta cta--edit cta--only-icon">
+              <span class="cta-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z"
+                  />
+                </svg>
+              </span>
+            </button>
+            <button class="cta cta--delete cta--only-icon">
+              <span class="cta-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+            </button>
+          </div>
+        </header>
+
+        <div class="episodios-item__description">
+          ${descricao}
+        </div>
+      </article>
+    </main>
+
+    <footer class="episodios-item__footer">
+      <figure class="episodios-item__audio-info">
+        <figcaption class="episodios-item__caption">
+          Escute o episódio
+          <a
+            href="${audio}"
+            >ou baixe o arquivo
+          </a>
+        </figcaption>
+        <audio
+          class="episodios-item__player"
+          controls
+          src="${audio}"
+        ></audio>
+      </figure>
+    </footer>
+  </li>
+`;
+
+const listEpisodios = (episodios) => {
+  const markup = episodios
+    .map((episodio) => gerarCardEpisodioMarkup(episodio))
+    .join('');
+
+  $listEpisodiosContainer.innerHTML = markup;
+};
+
+const getListEpisodios = async () => {
+  const endpoint = `${API_URL}/episodios`;
+
+  try {
+    const result = await fetch(endpoint);
+
+    if (!result.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const response = await result.json();
+    listEpisodios(response.episodios);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+getListEpisodios();
 /*
   --------------------------------------------------------------------------------------
   Função para manipular o dialog/modal
